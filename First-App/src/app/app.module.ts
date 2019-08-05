ff9d076eaaafbd3d8875a55325af94bd03acae24
Anirudha <anirudha.bagchi2011@gmail.com>
2019-08-05 21:41:02 +0530

    client component added & inherited component calling implemented

--- First-App/src/app/app.module.ts
+++ First-App/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ClientComponent } from './client/client.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule,
