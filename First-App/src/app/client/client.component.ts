beec9d9af3f76cd9e169c3ed3b8a5337523fe61e
Anirudha <anirudha.bagchi2011@gmail.com>
2019-08-06 23:00:06 +0530

    select element by class

--- First-App/src/app/client/client.component.ts
+++ First-App/src/app/client/client.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector : 'app-client',
+    //selector : 'app-client',
+    selector : '.app-client',
     styleUrls : ['./client.component.css'],
     template : 
     `<h3>This is from Client Component</h3>
