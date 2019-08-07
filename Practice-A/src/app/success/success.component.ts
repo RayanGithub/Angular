5c2fc81d72faecca95c13f709f558b8836251c6e
Anirudha <anirudha.bagchi2011@gmail.com>
2019-08-08 00:16:38 +0530

    practice A done :)

---
+++ Practice-A/src/app/success/success.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  templateUrl: './success.component.html',
+  styleUrls: ['./success-h3.component.css','./success-hr.component.css']
+})
+export class SuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
