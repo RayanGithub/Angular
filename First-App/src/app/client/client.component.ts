c539ac6fe0cdcef26d3ffa55e5fb72bd7f1134f1
Anirudha <anirudha.bagchi2011@gmail.com>
2019-08-08 23:56:32 +0530

    Property binding

--- First-App/src/app/client/client.component.ts
+++ First-App/src/app/client/client.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     //selector : 'app-client',
     selector : '.app-client',
     styleUrls : ['./client.component.css'],
+    templateUrl : './client.component.html'
+    /*
     template : 
     `<h3>This is from Client Component</h3>
     <app-server></app-server>`
-    //templateUrl : './client.component.html'
+    */
 })
 
-export class ClientComponent {
-    ClientData = 'data'
+export class ClientComponent  implements OnInit{
+    ClientData = 'data';
+    allowNewClient = false;
+
+    constructor() {
+        setTimeout(() => {
+            this.allowNewClient = true;
+        },2000)
+    }
+    ngOnInit() {
+
+    }
 }
